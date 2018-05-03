2f11183886994d71583723e458e98dc0514dfbeb
Pacheco <vascopac@hotmail.com>
2018-05-03 11:28:30 +0000

    Homepage alterada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Personal Finance</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #b3e6cc;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
@@ -49,7 +49,7 @@
                 font-size: 84px;
             }
 
-            .links > a {
+            .statistics > a {
                 color: #636b6f;
                 padding: 0 25px;
                 font-size: 12px;
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Personal Finance
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="statistics">
+                    <p>Total registered users: 0</p>
+                    <p>Total number of accounts: 0</p>
+                    <p>Movements registered on the plataform: 0</p>
                 </div>
             </div>
         </div>
